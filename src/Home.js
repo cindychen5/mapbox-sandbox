082d9486594dc8edde762c397dbf58dba07bd6e3
Cindy Chen <cchen4@cnm.edu>
2021-04-20 17:23:13 -0600

    got map up on page

---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import React from "react"
+import ReactMapGL from 'react-map-gl';
+import {Container} from "reactstrap";
+
+export function Home() {
+    const [points, setPoints] = React.useState([
+        {lat: 35.332, lng: -106.652},
+        {lat: 35.339, lng: -106.656},
+        {lat: 35.40, lng: -106.666},
+        {lat: 35.23, lng: -106.4444}
+    ]);
+    // center={[-106.65, 35.33]}
+
+    const [viewport, setViewport] = React.useState({
+        latitude: 35.33,
+        longitude: -106.65,
+        zoom: 5
+    });
+
+    return (
+        <>
+            <Container>
+                <h1>Here is the map</h1>
+                <ReactMapGL
+                    {...viewport}
+                    width="80vw"
+                    height="100vh"
+                    onViewportChange={(viewport) => setViewport(viewport)}
+                    mapStyle="mapbox://styles/mapbox/dark-v9"
+                    mapboxApiAccessToken={"pk.eyJ1IjoiY2ljaGVuNTgiLCJhIjoiY2ttM25wOWdsMTQxNzJxcXRlZDYxcm5saSJ9.wdnJVaMM3kOfsCuMeoB-vA"}
+                />
+
+            </Container>
+
+        </>
+    )
+}
